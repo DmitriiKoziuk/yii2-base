59b4befb40061312fc25c05db198c157ddf2aaf3
Kynica <dmitry.kozyuk@gmail.com>
2019-02-08 16:03:51 +0200

    Separate data and form.

---
+++ src/forms/Form.php
@@ -0,0 +1,19 @@
+<?php
+namespace DmitriiKoziuk\yii2Base\forms;
+
+use yii\base\Model;
+
+class Form extends Model
+{
+    public function getErrorsAsString(): string
+    {
+        $string = '';
+        foreach ($this->getErrors() as $errorName => $messages) {
+            $string .= $errorName . PHP_EOL;
+            foreach ($messages as $message) {
+                $string .= '-' . $message . PHP_EOL;
+            }
+        }
+        return $string;
+    }
+}

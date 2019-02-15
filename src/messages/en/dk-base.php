7d356983f1df94288939eda479cc6a8449aa0d60
Kynica <dmitry.kozyuk@gmail.com>
2019-02-15 10:31:35 +0200

    Add translation

--- src/messages/en/dk-base.php
+++ src/messages/en/dk-base.php
@@ -1,8 +1,11 @@
 <?php
 return [
+    'Create' => 'Create',
     'Save' => 'Save',
     'Update' => 'Update',
     'Delete' => 'Delete',
     'Created at' => 'Created at',
     'Updated at' => 'Updated at',
+    'Page not found.' => 'Page not found.',
+    'Are you sure you want to delete this item?' => 'Are you sure you want to delete this item?',
 ];

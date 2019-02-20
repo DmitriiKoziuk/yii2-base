08a9bbc34ae271ddf731e0a4196147ce1a606f79
Kynica <dmitry.kozyuk@gmail.com>
2019-02-20 12:39:18 +0200

    Add codeception tests

---
+++ tests/unit/helpers/UrlHelperTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace DmitriiKoziuk\yii2Base\tests\unit\helpers;
+
+use Codeception\Test\Unit;
+use DmitriiKoziuk\yii2Base\helpers\UrlHelper;
+
+class UrlHelperTest extends Unit
+{
+    /**
+     * @dataProvider dataProviderSlugFromString
+     * @param string $string
+     * @param string $expect
+     */
+    public function testGetSlugFromString(string $string, string $expect): void
+    {
+        expect('Slug string', UrlHelper::getSlugFromString($string))->equals($expect);
+    }
+
+    public function dataProviderSlugFromString(): array
+    {
+        return [
+            ['', ''],
+            ['Hello, world', 'hello-world'],
+            ['Apple iPhone Xs Max 64Gb Space Gray (MT502)', 'apple-iphone-xs-max-64gb-space-gray-mt502'],
+            [
+                '  Apple !@#$%^&*()  iPhone Xs---Max "64Gb" Space Gray (MT502)  ',
+                'apple-iphone-xs-max-64gb-space-gray-mt502'
+            ],
+        ];
+    }
+}

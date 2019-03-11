0a4882d2c8cb100e4c692f3315c06670da445b8a
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-03-11 10:56:54 +0200

    Update test

--- tests/unit/helpers/UrlHelperTest.php
+++ tests/unit/helpers/UrlHelperTest.php
@@ -19,12 +19,19 @@ class UrlHelperTest extends Unit
     public function dataProviderSlugFromString(): array
     {
         return [
-            ['', ''],
-            ['Hello, world', 'hello-world'],
-            ['Apple iPhone Xs Max 64Gb Space Gray (MT502)', 'apple-iphone-xs-max-64gb-space-gray-mt502'],
-            [
-                '  Apple !@#$%^&*()  iPhone Xs---Max "64Gb" Space Gray (MT502)  ',
+            'do nothing'     => ['', ''],
+            'simple string'  => ['Hello, world', 'hello-world'],
+            'typical string' => [
+                'Apple iPhone Xs Max 64Gb Space Gray (MT502)',
                 'apple-iphone-xs-max-64gb-space-gray-mt502'
+            ],
+            'hard string' => [
+                "  Apple !@#$%^&*() \r . iPhone Xs---Max \"64Gb\" Space Gray (MT502)  \n\t",
+                'apple-iphone-xs-max-64gb-space-gray-mt502'
+            ],
+            'With slash' => [
+                'Apple/iPhone Xs Max 64Gb Space Gray (MT502)',
+                'apple/iphone-xs-max-64gb-space-gray-mt502'
             ],
         ];
     }

ef7ccf76e8ea3ffe6f9a09641fda34a29c6f3412
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-10-02 14:24:23 +0300

    Remove non-usable code.

--- src/BaseModule.php
+++ src/BaseModule.php
@@ -1,55 +1,28 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace DmitriiKoziuk\yii2Base;
 
 use yii\base\Application as BaseApp;
-use yii\console\Application as ConsoleApp;
-use DmitriiKoziuk\yii2Base\helpers\UrlHelper;
-use DmitriiKoziuk\yii2Base\helpers\FileHelper;
 
 final class BaseModule extends \yii\base\Module
 {
     const ID = 'dk-base';
 
-    const TRANSLATE = self::ID;
-
-    /**
-     * @var \yii\di\Container
-     */
-    public $diContainer;
+    const TRANSLATION = self::ID;
 
     public function init()
     {
         /** @var BaseApp $app */
         $app = $this->module;
-        $this->_initLocalProperties($app);
-        $this->_registerTranslation($app);
-        $this->_registerClassesToDIContainer();
+        $this->registerTranslation($app);
     }
 
-    private function _initLocalProperties(BaseApp $app)
+    private function registerTranslation(BaseApp $app)
     {
-        if ($app instanceof ConsoleApp) {
-            $app->controllerMap['migrate']['class'] = 'yii\console\controllers\MigrateController';
-        }
-    }
-
-    private function _registerTranslation(BaseApp $app)
-    {
-        $app->i18n->translations[self::ID] = [
+        $app->i18n->translations[self::TRANSLATION] = [
             'class'          => 'yii\i18n\PhpMessageSource',
             'sourceLanguage' => 'en',
             'basePath'       => '@DmitriiKoziuk/yii2Base/messages',
         ];
-    }
-
-    private function _registerClassesToDIContainer()
-    {
-        $diContainer = \Yii::$container;
-        $diContainer->setSingleton(UrlHelper::class, function () {
-            return new UrlHelper();
-        });
-        $diContainer->setSingleton(FileHelper::class, function () {
-            return new FileHelper();
-        });
     }
 }
